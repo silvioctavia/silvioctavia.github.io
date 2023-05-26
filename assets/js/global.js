f131e6493746006dd7234f84384d7090c1385a2b
silvioctavia <129679169+silvioctavia@users.noreply.github.com>
2023-05-26 08:17:57 +0700

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -20,14 +20,14 @@ function welcomeMsg() {
     const welcomeMsgEl = document.querySelector("#welcomeMsg");
     let array = 0;
 
-    const takapedia = `<a href="https://takapedia.com/" target="_blank">takapedia</a>`;
-    const takapediaIG = `<a href="https://www.instagram.com/taka_pedia/" target="_blank">taka_pedia</a>`;
-    const takapediaWA = `<a href="https://api.whatsapp.com/send?phone=6287843920288" target="_blank">6287843920288</a>`;
+    const takapedia = `<a href="https://octashop.biz.id/" target="_blank">Octa Shop</a>`;
+    const takapediaIG = `<a href="https://octashop.biz.id" target="_blank">Octa Shop</a>`;
+    const takapediaWA = `<a href="https://api.whatsapp.com/send?phone=6285173326900" target="_blank">6285173326900</a>`;
 
     let arrayEl = [
-        `<p class="animation mb-0 text-truncate">Follow ig takapedia untuk ingfo flashsale</p>`,
-        `<p class="animation mb-0 text-truncate">IG: ${takapediaIG} / WA: ${takapediaWA}</p>`,
-        `<p class="animation mb-0 text-truncate">Gas langsung joki di ${takapedia} banh 😅☝️</p>`,
+        `<p class="animation mb-0 text-truncate">Jasa Gift Skin Termurah dan Terpercaya</p>`,
+        `<p class="animation mb-0 text-truncate">Website: ${takapediaIG} / WA Admin: ${takapediaWA}</p>`,
+        `<p class="animation mb-0 text-truncate">Gas langsung beli Skin di ${takapedia} 😍☝️</p>`,
     ];
     setInterval(() => {
         welcomeMsgEl.innerHTML = arrayEl[array];
